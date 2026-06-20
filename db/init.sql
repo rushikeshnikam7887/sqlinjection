218f6e049e368ba5978e4b586be361c874d88321
Rushikesh Nikam <94787496+rushikeshnikam7887@users.noreply.github.com>
2026-06-20 10:28:15 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,18 @@
 CREATE DATABASE IF NOT EXISTS testdb;
 USE testdb;
 
+CREATE TABLE account (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_no VARCHAR(20),
+    name VARCHAR(50),
+    lastname VARCHAR(50)
+);
+
+INSERT INTO account (account_no, name, lastname) VALUES
+('1001', 'Amit', 'Sharma'),
+('1002', 'Rahul', 'Patil'),
+('1003', 'Priya', 'Deshmukh');
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50),
@@ -11,15 +23,3 @@ INSERT INTO users (username, password) VALUES
 ('admin', 'admin123'),
 ('john', 'john123'),
 ('alice', 'alice123');
-
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-	lastname VARCHAR(100),
-    city VARCHAR(100)
-);
-
-INSERT INTO books (name, lastname, city) VALUES
-('Java Basics', 'ABC', 'Pearson'),
-('Cyber Security', 'XYZ', 'OReilly'),
-('Networks', 'PQR', 'McGrawHill');
